16e80679fc0d315654d37b17ddea01366826860c
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2020-03-04 17:13:30 +0530

    Add Ignore and Code Cleaned

--- gui.py
+++ gui.py
@@ -4,11 +4,18 @@ from tkinter import *
 window = Tk()
 
 window.title("File Collector")
-window.geometry("350x200")
-window.configure(background="#3E4149")
+window.geometry("300x100")
+window.configure(background="#3E4149", pady=10, padx=20)
 
-lbl = Label(window, foreground="white", text="Hello",
-            font=("Arial Bold", 24), background="#3E4149")
-lbl.grid(row=0, column=0)
+# Heading
+heading_TXT = Label(window, foreground="white", text="File Collector",
+                    font=("Arial Bold", 24), background="#3E4149")
+heading_TXT.pack()
+
+# Search Folder
+search_BTN = Button(window, text="Search Folder",
+                    highlightbackground="#3E4149", background="#3E4149", foreground="#FFFFFF")
+search_BTN.pack()
+
 
 window.mainloop()
