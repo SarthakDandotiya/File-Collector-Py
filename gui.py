55d0676e73b8da6c0bba749c634e36aac0c72f7a
SarthakDandotiya <sarthak.dandotiya23@gmail.com>
2020-03-06 19:19:28 +0530

    Update UI

--- gui.py
+++ gui.py
@@ -4,18 +4,43 @@ from tkinter import *
 window = Tk()
 
 window.title("File Collector")
-window.geometry("300x100")
-window.configure(background="#3E4149", pady=10, padx=20)
+# window.geometry("400x200")
+window.configure(background="#3E4149", pady=20, padx=40)
 
 # Heading
 heading_TXT = Label(window, foreground="white", text="File Collector",
                     font=("Arial Bold", 24), background="#3E4149")
 heading_TXT.pack()
 
-# Search Folder
-search_BTN = Button(window, text="Search Folder",
-                    highlightbackground="#3E4149", background="#3E4149", foreground="#FFFFFF")
-search_BTN.pack()
+# Input Search Directory path
+input_TXT = Label(window, foreground="#BDBDBD",
+                  text="Complete path of directory to search:", background="#3E4149", pady=5)
+input_TXT.pack()
+searchdir_INP = Entry(window, width=35)
+searchdir_INP.pack()
 
+# Input Destination Directory path
+output_TXT = Label(window, foreground="#BDBDBD",
+                   text="Complete path of directory to copy to:", background="#3E4149", pady=5)
+output_TXT.pack()
+destdir_INP = Entry(window, width=35)
+destdir_INP.pack()
+
+# Input file extension
+extension_TXT = Label(window, foreground="#BDBDBD",
+                      text="Copy files with the extension [without '.']:", background="#3E4149", pady=5)
+extension_TXT.pack()
+extension_INP = Entry(window, width=35)
+extension_INP.pack()
+
+# Run Button
+run_BTN = Button(window, text="Copy Files", font=("Arial Medium", 14),
+                 highlightbackground="#3E4149", background="#3E4149", foreground="#FFFFFF", pady=20, width=15)
+run_BTN.pack()
+
+# Status
+status_TXT = Label(window, foreground="#BDBDBD",
+                   text="Ready...", background="#3E4149")
+status_TXT.pack()
 
 window.mainloop()
